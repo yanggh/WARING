51100069061eabd6989d5496151f00da7ca49b73
yanggh <823457590@qq.com>
2016-12-03 04:49:53 -0500

    20161203

--- bash.sh
+++
@@ -1,2 +0,0 @@
-g++ -DHAVE_CONFIG_H  -I/home/yanggh/Documents/snmp++-3.3.7   -I/home/yanggh/Documents/snmp++-3.3.7/include $1.cpp -std=c++11   -lpthread /home/yanggh/Documents/snmp++-3.3.7/src/.libs/libsnmp++.so -Wl,-rpath -Wl,/usr/local/lib  -o  $1 -Wall 
-
